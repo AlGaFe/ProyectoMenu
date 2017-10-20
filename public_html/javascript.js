2ac56456e14cebe87201f12dd20e74bace184577
Alumno <Alumno@2A5PC06>
2017-10-20 10:09:40 +0200

    He realizado: Cambios varios

--- public_html/javascript.js
+++ public_html/javascript.js
@@ -1,7 +1,25 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+function calcularPrecio(){
+var checkBox=document.getElementsByTagName("input");
+total=0;
+document.getElementById("precio").innerHTML = total;
+    for (var i = 0; i < checkBox.length; i++) {
+        if(checkBox[i].checked==true){
+            var precio = parseFloat(checkBox[i].value);
+            total=precio+total;
+            document.getElementById("precio").innerHTML = total;
+        }
+    }
+}
+function hacerGrande(producto){
+     
+     var imagen = document.createElement("img");
+     var imagenProducto = producto;
+     imagen.src = imagenProducto.src;
+     imagen.className = "productoVisionar"; 
+    document.getElementById("imagenmuestra").src=imagen.src;
+}
+function hacerPequeña(producto){
+    document.getElementById("imagenmuestra").src="";
+    
+}
